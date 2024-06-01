7c2efc49b6780b4d724b3f13dbc4c6ff8f1fb073
AndriiS_Dev <mr.andrii.semenov@gmail.com>
2024-06-02 00:11:11 +0300

    module-3: order entities

---
+++ src/main/java/app/delivery/core/domain/courier/aggregate/Transport.java
@@ -0,0 +1,33 @@
+package app.delivery.core.domain.courier.aggregate;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.stream.Stream;
+
+@AllArgsConstructor
+@Getter
+public enum Transport {
+
+    PEDESTRIAN(0, "Pedestrian", 1, 1),
+    BICYCLE(1, "Bicycle", 2, 4),
+    SCOOTER(2, "Scooter", 3, 6),
+    CAR(3, "Car", 4, 8);
+
+    private final int id;
+    private final String name;
+    private final int speed;
+    private final int capacity;
+
+
+    public boolean canCarry(int weight) {
+        return capacity >= weight;
+    }
+
+    public static Transport of(String name) {
+        return Stream.of(Transport.values())
+                .filter(e -> e.getName().equals(name))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("Unknown Transport name: " + name));
+    }
+}
---
+++ src/main/java/app/delivery/core/domain/order/aggregate/Order.java
@@ -0,0 +1,48 @@
+package app.delivery.core.domain.order.aggregate;
+
+import app.delivery.core.shared.kernel.Location;
+import app.delivery.core.shared.kernel.Weight;
+import lombok.*;
+
+import java.util.UUID;
+
+@Builder
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@EqualsAndHashCode(of = "id")
+public class Order {
+
+    private UUID id;
+
+    private UUID courierId;
+
+    private OrderStatus status;
+
+    private Weight weight;
+
+    private Location location;
+
+
+    public static Order create(@NonNull UUID id, @NonNull Location location, @NonNull Weight weight) {
+        return Order.builder()
+                .id(id)
+                .location(location)
+                .weight(weight)
+                .status(OrderStatus.CREATED)
+                .build();
+    }
+
+    public void assignCourier(@NonNull UUID courierId) {
+        this.courierId = courierId;
+        this.status = OrderStatus.ASSIGNED;
+    }
+
+    public void complete() {
+        if (!OrderStatus.ASSIGNED.equals(this.status)) {
+            throw new IllegalStateException("Cannot complete order. Expected status to be ASSIGNED but was %s".formatted(this.status));
+        }
+        this.status = OrderStatus.COMPLETED;
+    }
+}
+
