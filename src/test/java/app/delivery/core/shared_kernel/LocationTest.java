2bb476150f4c6c95665f149b844fd7182c242dd9
AndriiS_Dev <andrey.s@billline.net>
2024-06-01 11:36:40 +0300

    module-2: resolve comments and corresponds to last changes test fixes

--- src/test/java/app/delivery/core/shared_kernel/LocationTest.java
+++ src/test/java/app/delivery/core/shared_kernel/LocationTest.java
@@ -3,46 +3,58 @@ package app.delivery.core.shared_kernel;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
+import static app.delivery.core.shared_kernel.Location.MINIMUM_COORDINATE;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class LocationTest {
+
+    private final int BELLOW_MIN_COORDINATE = 0;
+    private final int VALID_COORDINATE_FIVE = 5;
+    private final int ABOVE_MAX_VALID_COORDINATE = 11;
 
     @Nested
     class Create {
 
         @Test
         void givenValidCoordinate_thenSuccessfullyCompleted() {
-            Location location = Location.create(1.1, 1.1);
-
-            assertNotNull(location);
-            assertEquals(1.1, location.getXCoordinate());
-            assertEquals(1.1, location.getYCoordinate());
+            Location location = new Location(MINIMUM_COORDINATE, MINIMUM_COORDINATE);
+            assertAll(
+                    () -> assertNotNull(location),
+                    () -> assertEquals(MINIMUM_COORDINATE, location.getXCoordinate()),
+                    () -> assertEquals(MINIMUM_COORDINATE, location.getYCoordinate())
+            );
         }
 
         @Test
-        void givenBelowMinimumCoordinate_thenThrowException() {
+        void givenBelowMinimumXCoordinate_thenThrowException() {
             IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
-                Location.create(1.0, 1.1);
+                new Location(BELLOW_MIN_COORDINATE, MINIMUM_COORDINATE);
             });
-            assertEquals("xCoordinate is out of bounds", exception.getMessage());
-
-            exception = assertThrows(IllegalArgumentException.class, () -> {
-                Location.create(1.1, 1.0);
-            });
-            assertEquals("yCoordinate is out of bounds", exception.getMessage());
+            assertEquals("xCoordinate: %s is out of bounds".formatted(BELLOW_MIN_COORDINATE), exception.getMessage());
         }
 
         @Test
-        void givenAboveMaximumCoordinate_thenThrowException() {
+        void givenBelowMinimumYCoordinate_thenThrowException() {
             IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
-                Location.create(10.11, 1.1);
+                new Location(MINIMUM_COORDINATE, BELLOW_MIN_COORDINATE);
             });
-            assertEquals("xCoordinate is out of bounds", exception.getMessage());
+            assertEquals("yCoordinate: %s is out of bounds".formatted(BELLOW_MIN_COORDINATE), exception.getMessage());
+        }
 
-            exception = assertThrows(IllegalArgumentException.class, () -> {
-                Location.create(1.1, 10.11);
+        @Test
+        void givenAboveMaximumXCoordinate_thenThrowException() {
+            IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+                new Location(ABOVE_MAX_VALID_COORDINATE, MINIMUM_COORDINATE);
             });
-            assertEquals("yCoordinate is out of bounds", exception.getMessage());
+            assertEquals("xCoordinate: %s is out of bounds".formatted(ABOVE_MAX_VALID_COORDINATE), exception.getMessage());
+        }
+
+        @Test
+        void givenAboveMaximumYCoordinate_thenThrowException() {
+            IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+                new Location(MINIMUM_COORDINATE, ABOVE_MAX_VALID_COORDINATE);
+            });
+            assertEquals("yCoordinate: %s is out of bounds".formatted(ABOVE_MAX_VALID_COORDINATE), exception.getMessage());
         }
     }
 
@@ -52,28 +64,29 @@ public class LocationTest {
 
         @Test
         void givenValidDistance_thenSuccessfullyCompleted() {
-            Location location1 = Location.create(1.1, 1.1);
-            Location location2 = Location.create(2.2, 2.2);
+            int expectedDistance = 8; //5-1 + 5-1
+            Location location1 = new Location(MINIMUM_COORDINATE, MINIMUM_COORDINATE);
+            Location location2 = new Location(VALID_COORDINATE_FIVE, VALID_COORDINATE_FIVE);
 
-            double distance = location1.calculateDistance(location2);
-            assertEquals(2.2, distance);
+            int distance = location1.calculateDistance(location2);
+            assertEquals(expectedDistance, distance);
         }
     }
 
     @Nested
     class Equals {
 
-        Location location1 = Location.create(1.1, 1.1);
+        Location location1 = new Location(MINIMUM_COORDINATE, MINIMUM_COORDINATE);
 
         @Test
         void givenSameCoordinates_thenAssertEqual() {
-            Location location2 = Location.create(location1.getXCoordinate(), location1.getYCoordinate());
+            Location location2 = new Location(location1.getXCoordinate(), location1.getYCoordinate());
             assertEquals(location1, location2);
         }
 
         @Test
         void givenDifferentCoordinates_thenAssertNotEqual() {
-            Location location2 = Location.create(2.2, 2.2);
+            Location location2 = new Location(VALID_COORDINATE_FIVE, VALID_COORDINATE_FIVE);
             assertNotEquals(location1, location2);
         }
     }
--- src/test/java/app/delivery/core/shared_kernel/WeightTest.java
+++ src/test/java/app/delivery/core/shared_kernel/WeightTest.java
@@ -3,6 +3,7 @@ package app.delivery.core.shared_kernel;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
+import static app.delivery.core.shared_kernel.Weight.MINIMUM_WEIGHT;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class WeightTest {
@@ -12,42 +13,67 @@ public class WeightTest {
 
         @Test
         void givenValidWeight_thenSuccessfullyCompleted() {
-            Weight weight = Weight.create(10);
-
-            assertNotNull(weight);
-            assertEquals(10, weight.getKilograms());
+            Weight weight = new Weight(10);
+            assertAll(
+                    () -> assertNotNull(weight),
+                    () -> assertEquals(10, weight.getKilograms())
+            );
         }
 
         @Test
-        void givenNegativeOrZeroWeight_thenThrowException() {
-            IllegalArgumentException negativeWeightException = assertThrows(IllegalArgumentException.class, () -> {
-                Weight.create(-1);
-            });
+        void givenNegativeWeight_thenThrowException() {
+            assertTrue(assertThrows(IllegalArgumentException.class, () -> {
+                new Weight(-1);
+            }).getMessage().startsWith("Weight should be greater than " + MINIMUM_WEIGHT));
+        }
 
-            IllegalArgumentException zeroWeightException = assertThrows(IllegalArgumentException.class, () -> {
-                Weight.create(0);
-            });
-
-            assertTrue(negativeWeightException.getMessage().startsWith("Weight should be greater than"));
-            assertTrue(zeroWeightException.getMessage().startsWith("Weight should be greater than"));
+        @Test
+        void givenZeroWeight_thenThrowException() {
+            assertTrue(assertThrows(IllegalArgumentException.class, () -> {
+                new Weight(0);
+            }).getMessage().startsWith("Weight should be greater than " + MINIMUM_WEIGHT));
         }
     }
 
     @Nested
     class Equals {
 
-        Weight weight1 = Weight.create(10);
+        Weight weight1 = new Weight(10);
 
         @Test
         void givenSameWeights_thenAssertEqual() {
-            Weight weight2 = Weight.create(weight1.getKilograms());
+            Weight weight2 = new Weight(weight1.getKilograms());
             assertEquals(weight1, weight2);
         }
 
         @Test
         void givenDifferentCoordinates_thenAssertNotEqual() {
-            Weight weight2 = Weight.create(20);
+            Weight weight2 = new Weight(20);
             assertNotEquals(weight1, weight2);
+        }
+    }
+
+    @Nested
+    class Compare {
+        @Test
+        void givenTwoEqualWeights_whenComparing_thenResultIsZero() {
+            Weight weight1 = new Weight(5.0);
+            Weight weight2 = new Weight(5.0);
+            assertEquals(0, weight1.compareTo(weight2));
+        }
+
+        @Test
+        void givenFirstWeightLighter_whenComparing_thenResultIsNegative() {
+            Weight weight1 = new Weight(5.0);
+            Weight weight2 = new Weight(10.0);
+            assertTrue(weight1.compareTo(weight2) < 0);
+        }
+
+        @Test
+        void givenFirstWeightHeavier_whenComparing_thenResultIsPositive() {
+            Weight weight1 = new Weight(10.0);
+            Weight weight2 = new Weight(5.0);
+            assertTrue(weight1.compareTo(weight2) > 0);
         }
     }
 }
