a399d073ba2847977896f197c716bbd6613d55be
AndriiS_Dev <mr.andrii.semenov@gmail.com>
2024-06-30 16:28:27 +0300

    module-4: fixed lost connection exception

--- src/test/java/app/delivery/configs/ContainersEnvironment.java
+++ src/test/java/app/delivery/configs/ContainersEnvironment.java
@@ -1,10 +1,12 @@
 package app.delivery.configs;
 
 import app.delivery.PostgresTestContainer;
+import org.springframework.test.annotation.DirtiesContext;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
 @Testcontainers
 public class ContainersEnvironment {
 
